325b44fed444997b6b10b0a4198a2ad9de855f58
Roboe <roboe@openmailbox.org>
2015-06-01 23:51:33 +0200

    Minor changes and new words

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-(function (global, undefined) {
-	//'use strict';
+(function (global) {
+	'use strict';
 
 	function ajax(post_get, path, mimetype, send, callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.open(post_get, path, true);
 		xhr.overrideMimeType(mimetype);
 		xhr.onreadystatechange = function () {
-			if (xhr.readyState == 4) {
-				if (xhr.status == 200) {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
 					callback(null, xhr.responseText);
 				} else {
 					callback(xhr.status);
@@ -29,24 +29,26 @@
 		});
 	}
 
-	function r(array) {
+	function arrayndomize(array) {
 		return array[Math.floor(Math.random() * array.length)];
 	}
 
-	var $ = document.querySelector.bind(document);
-	var colors = ["blue", "orange", "green", "dark", "purple"]; // CSS <html> classnames
-	var html = $("html");
-	var word = $("#word");
-	var meaning = $("#meaning");
+	/* Constants */
+	var $ = document.querySelector.bind(document),
+		COLORS = ["blue", "orange", "green", "dark", "purple"], // CSS <html> classnames
+		DB = "js/db.json",
 
-	getJSON("js/db.json", function (json) {
-		//console.log(json);
+	/* Variables */
+		html = $("html"),
+		word = $("#word"),
+		meaning = $("#meaning");
+
+	getJSON(DB, function (json) {
 		if (json.words) {
-			var w = r(json.words);
-			console.log(w);
-			word.textContent = w.word;
-			meaning.textContent = w.meaning;
-			html.className = (!!w.color) ? w.color : r(colors);
+			var choosenWord = arrayndomize(json.words);
+			word.textContent = choosenWord.word + ".";
+			meaning.textContent = choosenWord.meaning;
+			html.className = (!!choosenWord.color) ? choosenWord.color : arrayndomize(COLORS);
 		}
 	});
 
