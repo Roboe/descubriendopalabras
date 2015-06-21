0c34cfeb25b29ff8812a9f9cc28d7f8374dc23b5
Roboe <roboe@openmailbox.org>
2015-06-21 20:30:46 +0200

    Add hashed-based word search

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function (global) {
 	'use strict';
 
+	/* Utils */
 	function ajax(post_get, path, mimetype, send, callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.open(post_get, path, true);
@@ -33,23 +34,47 @@
 		return array[Math.floor(Math.random() * array.length)];
 	}
 
+	function getHashWord() {
+		return global.location.hash.substr(1); // Return empty string if not available
+	}
+
 	/* Constants */
 	var $ = document.querySelector.bind(document),
+		log = console.log.bind(console),
 		COLORS = ["blue", "orange", "green", "dark", "purple"], // CSS <html> classnames
 		DB = "js/db.json",
 
 	/* Variables */
 		html = $("html"),
-		word = $("#word"),
-		meaning = $("#meaning");
+		word_element = $("#word"),
+		meaning_element = $("#meaning"),
+		hashWord,
+		choosenWord;
 
-	getJSON(DB, function (json) {
-		if (json.words) {
-			var choosenWord = arrayndomize(json.words);
-			word.textContent = choosenWord.word + ".";
-			meaning.textContent = choosenWord.meaning;
-			html.className = (!!choosenWord.color) ? choosenWord.color : arrayndomize(COLORS);
-		}
-	});
+	/* Functions */
+	function showWord(word) {
+		word_element.textContent = word.word + ".";
+		meaning_element.textContent = word.meaning;
+		html.className = (!!word.color && COLORS.indexOf(word.color)) ? word.color : arrayndomize(COLORS.filter(function (color) {
+			return color != html.className;
+		}));
+	}
 
+	function onLoad() {
+		hashWord = getHashWord();
+		getJSON(DB, function (json) {
+			if (json.words) {
+				if (hashWord) {
+					choosenWord = json.words.filter(function(word) {
+						return word.meta.slug === hashWord;
+					})[0];
+				}
+				choosenWord = choosenWord || arrayndomize(json.words);
+				showWord(choosenWord);
+			}
+		});
+	}
+
+	global.addEventListener("DOMContentLoaded", onLoad, false);
+	global.addEventListener("hashchange", onLoad, false);
 }(this));
