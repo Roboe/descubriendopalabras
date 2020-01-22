43af2e6adba534fb649c96b13abbd2338d79eeff
Roberto MF <git@virgulilla.com>
2020-01-22 18:34:16 +0100

    Load random color on each load (with JS available)

--- layouts/_default/list.js.js
+++ layouts/_default/list.js.js
@@ -1,18 +1,30 @@
 'use strict'
 
 const currentPage = window.location.href;
+const htmlElementClasses = document.querySelector('html').classList
 const pages = [
 {{- range .RegularPages }}
   "{{ .Permalink }}",
 {{- end }}
 ]
+const colorClasses = [
+  {{- range .Site.Data.bg_colors }}
+  "{{ . }}",
+  {{- end }}
+]
 
+
+function getRandomItemFromList(list) {
+  if (list.length === 0) return undefined
+
+  const randomIndex = Math.floor(Math.random() * list.length)
+  return list[randomIndex]
+}
 
 function getRandomPageUrl() {
   if (pages.length <= 1) return currentPage
 
-  const randomIndex = Math.floor(Math.random() * pages.length)
-  const randomPageUrl = pages[randomIndex]
+  const randomPageUrl = getRandomItemFromList(pages)
 
   return (randomPageUrl !== currentPage)
     ? randomPageUrl
@@ -28,3 +40,15 @@ function loadRandomPage(keepHistory = true) {
     window.location.replace(randomPageUrl)
   }
 }
+
+function loadRandomColorClass() {
+  if (htmlElementClasses.contains('has-color')) return
+
+  const randomColor = getRandomItemFromList(colorClasses)
+
+  htmlElementClasses.remove(...colorClasses)
+  htmlElementClasses.add(randomColor)
+}
+
+// Main
+loadRandomColorClass()
